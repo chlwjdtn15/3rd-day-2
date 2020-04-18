9942a829149382bec7570de8a82598f2746f3308
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-04-18 04:18:53 +0000

    해냄

--- Main.java
+++ Main.java
@@ -16,7 +16,7 @@ class Main {
     // 문제 : 1부터 3까지 출력하는 작업을 10번 해주세요.
     // 조건 : 2중 while문 사용
     
-    
+    /*
     int r = 1;
     int i = 1;
 
@@ -31,5 +31,27 @@ class Main {
       r++;
 
     }
+    */
+    
+    //for 문 구구단 8단
+
+    for ( int i = 1 ; i <= 9 ; i++){
+
+      int j = 8;
+      System.out.println( j + " x " + i + " = " + j * i);
+      
+    }
+
+    //for 문 (2중) 구구단 2~9단 (컴퓨터는 위에서 아래로 읽으니 조심!)
+
+  for (int x = 1 ; x <= 9 ; x++){ 
+
+    System.out.printf("%d 단: \n", x);
+
+    for ( int i = 1 ; i <= 9 ; i++){
+
+      System.out.println( x + " x " + i + " = " + x * i);   
+    }
+  }
   }
 }
