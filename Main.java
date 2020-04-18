70f57540d556b726f7d210a9c9093de9d197ba22
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-04-18 03:43:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+class Main {
+  public static void main(String[] args) {
+    //문제 100부터 25까지의 합 출력
+
+    /*
+    int i = 100;
+    int sum = 0;
+    while ( i >= 25 ){
+      sum += i;
+      i--;
+    } 
+    System.out.println( sum );
+
+    */
+
+    // 문제 : 1부터 3까지 출력하는 작업을 10번 해주세요.
+    // 조건 : 2중 while문 사용
+    
+    
+    int r = 1;
+    int i = 1;
+
+    while ( r <= 10 ){  
+
+        while ( i <= 3 ){
+            System.out.println( i );
+            i++;
+          }
+
+
+      r++;
+
+    }
+  }
+}
